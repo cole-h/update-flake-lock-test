f5ffa2a46846014a26d0f2a5a376cc05ba308792
Cole Helbling <cole.e.helbling@outlook.com>
2021-10-18 11:44:44 -0700

    update flake.nix without updating lockfile

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "test";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
   };
 
   outputs =
